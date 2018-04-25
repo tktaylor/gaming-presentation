c28372691ed8e4a6dddcd30a1c258684fb65ca4a
Tiffany Taylor <webmaster@jwcc.edu>
2018-04-25 09:38:35 -0500

    revert to previous commit, add slideshow functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+var slides = document.querySelectorAll('.content .slide');
+var currentSlide = 0;
+
+function nextSlide(){
+  goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+  goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+next.onClick = function(){
+  nextSlide();
+}
+previous.onClick = function(){
+  previousSlide();
+}
+
+document.onkeydown = function(e){
+  switch (e.keyCode){
+    case 37:
+      previousSlide();
+      break;
+    case 39:
+      nextSlide();
+      break;
+  }
+};
